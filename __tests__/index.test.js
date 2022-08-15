3c4b92a3166a75b47d93189645596808cb023b1c
Kwenoth <kwenoth@gmail.com>
2022-08-15 20:53:42 +0400

    Trying jest with __dirname

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,5 +1,13 @@
-import genDiff, { readFile } from '../src/index.js';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+import fs from 'fs';
+import genDiff from '../src/index.js';
 
-test('testing gendiff', () => {
-    expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toEqual(readFile('__fixtures__/example.txt'));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const getFixturePath = (filename) => path.resolve(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+
+test('testing comparison of two flat JSON files', () => {
+  expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toEqual(readFile('example.txt'));
 });
