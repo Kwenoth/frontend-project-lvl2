1bf5027b40cefcfc7d14d92acca8348dec4237b1
Kwenoth <kwenoth@gmail.com>
2022-09-05 21:34:15 +0400

    Edited plain.js

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,37 +1,39 @@
 import _ from 'lodash';
 
 const property = (value) => {
-  if (Array.isArray(value) || _.isPlainObject(value)) {
+  if (_.isPlainObject(value)) {
     return '[complex value]';
   }
   return typeof value === 'string' ? `'${value}'` : value;
 };
 
 const plain = (tree, path = '') => {
-  const result = tree.value.flatMap((node) => {
-    const {
-      key,
-      value,
-      value1,
-      value2,
-      status,
-    } = node;
+  const {
+    key,
+    value,
+    value1,
+    value2,
+    status,
+  } = tree;
 
-    const adress = `${path}.${key}`;
+  const name = key === undefined ? '' : key;
+  const adress = `${path}.${name}`;
 
-    switch (status) {
-      case 'updated':
-        return `Property '${adress.slice(1)}' was updated. From ${property(value1)} to ${property(value2)}`;
-      case 'removed':
-        return `Property '${adress.slice(1)}' was removed`;
-      case 'added':
-        return `Property '${adress.slice(1)}' was added with value: ${property(value)}`;
-      default:
-        return Array.isArray(value) ? `${plain({ value }, adress)}` : [];
+  switch (status) {
+    case 'root':
+    case 'nested': {
+      const result = value.flatMap((node) => plain(node, adress));
+      return result.join('\n');
     }
-  });
-
-  return result.join('\n');
+    case 'updated':
+      return `Property '${adress.slice(2)}' was updated. From ${property(value1)} to ${property(value2)}`;
+    case 'removed':
+      return `Property '${adress.slice(2)}' was removed`;
+    case 'added':
+      return `Property '${adress.slice(2)}' was added with value: ${property(value)}`;
+    default:
+      return [];
+  }
 };
 
 export default plain;
