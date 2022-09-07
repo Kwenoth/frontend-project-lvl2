026212b88eac9829e683f72158f781d9f0732c87
Kwenoth <kwenoth@gmail.com>
2022-09-07 14:42:20 +0400

    Fixed stylish.js

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,18 +1,18 @@
 import _ from 'lodash';
 import decompose from '../decompose.js';
 
-const getValue = (value, depth) => {
+const getValue = (value, func, depth, char) => {
   if (_.isPlainObject(value)) {
     const result = decompose(value);
-    const res = result.map((node) => stylish(node, depth)).join('\n');
-    return `{\n    ${res}\n        }`;
+    const res = result.map((node) => func(node, depth + 2)).join('\n');
+    return `{\n${res}\n${char.repeat(depth + 2)}}`;
   }
   return value;
 };
 
 const stylish = (tree, depth = 0) => {
   const indent = ' ';
-  const reps = depth + 1;
+  const reps = depth + 2;
   const {
     key,
     value,
@@ -23,21 +23,21 @@ const stylish = (tree, depth = 0) => {
 
   switch (status) {
     case 'root': {
-      const result = value.map((node) => stylish(node, reps));
+      const result = value.map((node) => stylish(node, 0));
       return `{\n${result.join('\n')}\n}`;
     }
     case 'nested': {
-      const result = value.map((node) => stylish(node, reps));
-      return `${indent.repeat(reps)}  ${key}: {\n${result.join('\n')}\n${indent.repeat(reps)}}`;
+      const result = value.map((node) => stylish(node, reps + 2));
+      return `${indent.repeat(reps)}  ${key}: {\n${result.join('\n')}\n${indent.repeat(reps + 2)}}`;
     }
     case 'updated':
-      return `${indent.repeat(reps)}- ${key}: ${getValue(value1)}\n${indent.repeat(reps)}+ ${key}: ${getValue(value2)}`;
+      return `${indent.repeat(reps)}- ${key}: ${getValue(value1, stylish, reps, indent)}\n${indent.repeat(reps)}+ ${key}: ${getValue(value2, stylish, reps, indent)}`;
     case 'removed':
-      return `${indent.repeat(reps)}- ${key}: ${getValue(value, reps)}`;
+      return `${indent.repeat(reps)}- ${key}: ${getValue(value, stylish, reps, indent)}`;
     case 'added':
-      return `${indent.repeat(reps)}+ ${key}: ${getValue(value, reps)}`;
+      return `${indent.repeat(reps)}+ ${key}: ${getValue(value, stylish, reps, indent)}`;
     case 'unchanged':
-      return `${indent.repeat(reps)}  ${key}: ${getValue(value)}`;
+      return `${indent.repeat(reps)}  ${key}: ${getValue(value, stylish, reps, indent)}`;
     default:
       throw new Error(`Неверный статус объекта 'diff': 'status: ${status}'`);
   }
