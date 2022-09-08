525959fa7a691284df018555ce14c010dcb3d22e
Kwenoth <kwenoth@gmail.com>
2022-09-08 15:11:47 +0400

    Implemented creation of an address using an array

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -7,7 +7,7 @@ const getProperty = (value) => {
   return _.isString(value) ? `'${value}'` : value;
 };
 
-const plain = (tree, path = '') => {
+const plain = (tree, path = []) => {
   const {
     key,
     value,
@@ -15,19 +15,20 @@ const plain = (tree, path = '') => {
     value2,
     status,
   } = tree;
-  const name = _.isUndefined(key) ? '' : key;
-  const adress = `${path}.${name}`;
+
+  const adress = [...path, key];
 
   switch (status) {
     case 'root':
+      return value.flatMap((node) => plain(node, [])).join('\n');
     case 'nested':
       return value.flatMap((node) => plain(node, adress)).join('\n');
     case 'updated':
-      return `Property '${adress.slice(2)}' was updated. From ${getProperty(value1)} to ${getProperty(value2)}`;
+      return `Property '${adress.join('.')}' was updated. From ${getProperty(value1)} to ${getProperty(value2)}`;
     case 'removed':
-      return `Property '${adress.slice(2)}' was removed`;
+      return `Property '${adress.join('.')}' was removed`;
     case 'added':
-      return `Property '${adress.slice(2)}' was added with value: ${getProperty(value)}`;
+      return `Property '${adress.join('.')}' was added with value: ${getProperty(value)}`;
     case 'unchanged':
       return [];
     default:
